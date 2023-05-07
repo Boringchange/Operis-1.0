2160d7b078c575336033d5e85d131b3abaaf65b2
Boringchange <omar_jv.s@hotmail.com>
2023-05-06 23:43:03 -0600

    .
    .

--- SRC/app.js
+++ SRC/app.js
@@ -23,7 +23,7 @@ app.use(myconnection(mysql, {
    host: 'localhost',
    user: 'root',
    password: 'n0m3l0',
-   port: 3308,
+   port: 3306,
    database: 'NEGOCIO3'
 }, 'single'));
 app.use(cookieParser('culosnegros123456789'));
